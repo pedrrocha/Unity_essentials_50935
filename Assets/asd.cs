0a00da63718f644f99386044ad60520bf12a6f29
pedrrocha <126675591+pedrrocha@users.noreply.github.com>
2023-03-17 18:56:52 +0000

    codding

--- Assets/asd.cs
+++ Assets/asd.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class new : MonoBehaviour
-{
-    public string myMessage; 
+public class asd : MonoBehaviour
+
+{   public Vector3 rotateChange;
+
     // Start is called before the first frame update
     void Start()
     {
-     
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-      Debug.log("mymessage")   
+        transform.Rotate (rotateChange);
     }
 }
